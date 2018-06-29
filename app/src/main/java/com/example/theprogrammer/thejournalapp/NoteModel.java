a620328d6f3d76649bbc9c44d2f00c2ea8c9e1d8
amon kats <amonkats94@gmail.com>
2018-06-29 18:27:46 +0300

    initial commit

---
+++ app/src/main/java/com/example/theprogrammer/thejournalapp/NoteModel.java
@@ -0,0 +1,33 @@
+package com.example.theprogrammer.thejournalapp;
+
+/**
+ * Created by The Programmer on 25-6-2018.
+ */
+
+public class NoteModel {
+
+    public String noteTitle;
+    public String noteTime;
+
+
+    public NoteModel(String noteTitle, String noteTime) {
+        this.noteTitle = noteTitle;
+        this.noteTime = noteTime;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteTime() {
+        return noteTime;
+    }
+
+    public void setNoteTime(String noteTime) {
+        this.noteTime = noteTime;
+    }
+}
